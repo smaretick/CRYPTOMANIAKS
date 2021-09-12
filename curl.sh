f500e27050caab0ede63ec1c68bfed645ff023a7
smaretick <scottmaretick51@gmail.com>
2021-09-11 19:40:52 -0500

    Add files via upload

---
+++ curl.sh
@@ -0,0 +1,4 @@
+#WORKS 09/08/2021
+#Need Subject
+
+curl --url "smtps://smtp.gmail.com:465" --mail-from "scottmaretick51@gmail.com" --mail-rcpt "scottmaretick51@gmail.com" --ssl --user "scottmaretick51@gmail.com:Sm110751$" --upload-file "/Users/scott/Desktop/curl.txt"
